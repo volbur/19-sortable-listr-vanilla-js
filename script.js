d19803e9339fd931d4b23f00f0d5a38d5d75c60c
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-06-03 18:18:06 +0300

    [js] created store listitems and dragStartIndex

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-'use stricts';
+"use stricts";
 
-const draggable_list = document.getElementById('draggable-list');
-const check = document.getElementById('check');
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
