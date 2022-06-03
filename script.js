cfe5573a417a2b91c718977632268710b51eb2ec
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-06-03 18:40:42 +0300

    [js] Insert list items into DOM

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ const richestPeople = [
 const listItems = [];
 
 let dragStartIndex;
+
+createList();
+
+function createList() {
+  [...richestPeople]
+    .map(a => ({value: a, sort: Math.random}))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+
+      listItem.setAttribute('data-index', index);
+
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+
+      listItem.push(listItem);
+
+      draggable_list.appendChild(listItem);
+    });
+}
